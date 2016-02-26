c072f04c2aebd7f97aa40cb4fdb4a8a7bb5629d6
persja40 <persja40@gmail.com>
2016-02-26 15:52:06 +0100

    Moja wizja klas
    Jedna krowka, ktora w sobie ma metody do generacji, wczytywania itd...

--- Graphs/Data/GraphMatrix.cs
+++ Graphs/Data/GraphMatrix.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Graphs.Data
 {
-    public class GraphMatrix{
-        public GraphMatrix(int nr){
-            nodesNr = nr;
-            connect = new int[nr, nr];
-        }
+    public class GraphMatrix
+    {
+        public void generator1(int nodes, int branches)
+        {
+            nodesNr = nodes;
+            connect = new int[nodes, nodes];//default =0
+            //TU OSTATNIO
 
+
+        }
+        public void generator2(int nodes, double prob)
+        {
+            if (0 <= prob || prob <= 1 || nodes < 2)
+                return;
+            nodesNr = nodes;
+            connect = new int[nodes, nodes];//default =0
+            Random r = new Random();
+            for (int i = 1; i < nodesNr; i++)
+                for (int j = 1; j < i; j++)
+                    if (r.Next(10000000) / 10000000 >= prob)
+                        connect[i, j] = connect[j, i] = 1;
+        }
+        public void readFromFile() {
+            //???
+        }
         private int[,] connect;
         private int nodesNr;
     }
