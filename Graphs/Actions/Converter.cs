fc7ba47debcd51a54ff94a29f744a5144c136610
Damian Łączak <Damian czak>
2016-02-27 16:49:55 +0100

    empty class for converter

---
+++ Graphs/Actions/Converter.cs
@@ -0,0 +1,28 @@
+﻿using Graphs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Actions
+{
+    public static class Converter
+    {
+        public static GraphMatrix ConvertToMatrix(object from)
+        {
+            throw new NotImplementedException();
+        }
+        public static object ConvertToX(object from)
+        {
+            throw new NotImplementedException();
+        }
+   
+        public static object ConvertToY(object from)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
