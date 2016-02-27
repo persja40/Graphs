59705ab0f1eff060b4b3cd42607511fc04a2afde
persja40 <persja40@gmail.com>
2016-02-27 14:41:59 +0100

    Generatory
    Dodałem obydwa generatory dla reprezentacji macierzowej.
    Dla innych nie będziemy pisąć tylko konwertowac z tej,
    zeby za duzo kodu nie bylo.

--- Graphs/Data/GraphMatrix.cs
+++ Graphs/Data/GraphMatrix.cs
@@ -9,15 +9,35 @@ namespace Graphs.Data
 {
     public class GraphMatrix
     {
-        public void generator1(int nodes, int branches)
+        public void generatorGER(int nodes, int branches)//generator Erdoesa-Renyiego
         {
             nodesNr = nodes;
-            connect = new int[nodes, nodes];//default =0
-            //TU OSTATNIO
-
-
+            connect = new int[nodesNr, nodesNr];//default =0
+            int max = (nodesNr * nodesNr - nodesNr) / 2;
+            int[] ar = new int[branches];
+            Random r = new Random();
+            for (int i = 0; i < nodesNr; i++)//losowanie wezlow
+                while (true)//sprawdzanie czy sie nie powtarzaja
+                {
+                    ar[i] = r.Next(max);
+                    bool q = false;
+                    for (int k = 0; k < i; k++)
+                        if (ar[k] == ar[i])
+                            q = true;
+                    if (!q)
+                        break;
+                }
+            int counter = 0;
+            for (int i = 1; i < nodesNr; i++)//wpisywanie wylosowanych liczb
+                for (int j = 1; j < i; j++)
+                {
+                    for (int k = 0; k < branches; k++)                    
+                        if (ar[k] == counter)
+                            connect[i, j] = connect[j, i] = 1;                    
+                    counter++;
+                }
         }
-        public void generator2(int nodes, double prob)
+        public void generatorGnp(int nodes, double prob)//generator G(n,p)
         {
             if (0 <= prob || prob <= 1 || nodes < 2)
                 return;
@@ -26,10 +46,11 @@ namespace Graphs.Data
             Random r = new Random();
             for (int i = 1; i < nodesNr; i++)
                 for (int j = 1; j < i; j++)
-                    if (r.Next(10000000) / 10000000 >= prob)
+                    if (r.Next(10000000) / 10000000 < prob)
                         connect[i, j] = connect[j, i] = 1;
         }
-        public void readFromFile() {
+        public void readFromFile()
+        {
             //???
         }
         private int[,] connect;
