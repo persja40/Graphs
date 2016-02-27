0a6887830b7855571c5ee2aeb912405dde14649b
Damian Łączak <Damian czak>
2016-02-27 15:55:19 +0100

    Added skeleton of FileLoad Class

---
+++ Graphs/Actions/FileLoad.cs
@@ -0,0 +1,33 @@
+﻿using Graphs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Actions
+{
+    public static class FileLoad
+    {
+        /// <summary>
+        /// Laduje graf w postaci macierzy z podanego pliku
+        /// </summary>
+        /// <param name="path">Sciezka do pliku z roszerzeniem *.matrix</param>
+        /// <returns>Graf macierzowy</returns>
+        public static GraphMatrix LoadMatrix(string path)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Zapisuje graf macierzowy do pliku
+        /// </summary>
+        /// <param name="graph">graf ktory bedzie zapisany</param>
+        /// <param name="path">sciezka do pliku w ktorym zostanie zapisany z rozszerzeniem *.matrix</param>
+        public static void SaveMatrix(GraphMatrix graph, string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        //i 2 inne metody do ladowania 2 innych typow grafow
+    }
+}
