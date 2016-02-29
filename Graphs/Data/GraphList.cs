efec7882388cab8e4b174a61526a5e85f2de1dc4
persja40 <persja40@gmail.com>
2016-02-29 10:57:56 +0100

    GraphList

--- Graphs/Data/GraphList.cs
+++ Graphs/Data/GraphList.cs
@@ -8,5 +8,21 @@ namespace Graphs.Data
 {
     public class GraphList
     {
+        public GraphList(int nodes)
+        {
+            connectNr = nodes;
+            connect = new List<int>[nodes];
+        }
+        public void MakeConnection(int node1, int node2)
+        {
+            connect[node1].Add(node2);
+            connect[node2].Add(node1);
+        }
+        public bool GetConnection(int node1, int node2)
+        {
+            return connect[node1].Contains(node2);
+        }
+        private List<int>[] connect;
+        private int connectNr;
     }
 }
