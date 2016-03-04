3c1528b270807b6a6063ac761b41488079f58d5f
Damian <lubiebardzozelki@gmail.com>
2016-03-04 17:50:23 +0100

    Added sandbox area :)

---
+++ Piaskownica/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piaskownica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
