8dd376b97e68ad987db9f0c817a23c86fa9794e4
Damian <lubiebardzozelki@gmail.com>
2016-03-05 01:54:47 +0100

    Task for Kamil

---
+++ Graphs/Actions/Misc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Actions
+{
+    public class Misc
+    {
+        /// <summary>
+        /// Sprawdza czy graf z danymi stopniami wierzchołków może istnieć
+        /// Algorytm : https://en.wikipedia.org/wiki/Havel%E2%80%93Hakimi_algorithm
+        /// </summary>
+        /// <param name="degrees">lista ze stopniami wierzchołków</param>
+        /// <returns>true jesli istnieje</returns>
+        public static bool Exists(List<int> degrees)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
