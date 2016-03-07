6ea4551c6ffeb51b37b4d495d9894b94b14b5f75
4papiez <4papiez@fis.agh.edu.pl>
2016-03-07 23:55:45 +0100

    4 funkcje zapisu do pliku

--- Graphs/Actions/GraphLoad.cs
+++ Graphs/Actions/GraphLoad.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Graphs.Actions
 {
@@ -16,7 +17,33 @@ namespace Graphs.Actions
         /// <returns>Graf macierzowy</returns>
         public static GraphMatrix LoadMatrix(string path)
         {
-            throw new NotImplementedException();
+            if (!File.Exists(path))
+            {
+
+                return new GraphMatrix(0);
+            }
+            //throw new NotImplementedException();
+            StreamReader sr = new StreamReader(path);
+            string s = sr.ReadLine();
+            int ile = s.Length;
+            int[,] tab = new int [ile,ile];
+            string[] dane=new string[ile];
+            dane[0]=s;
+            for (int i = 1; i < ile; i++)
+            {
+                dane[i] = sr.ReadLine();
+            }
+            for (int i = 0; i < ile; i++)
+            {
+                for (int j = 0; j < ile; j++)
+                {
+                    if (dane[i][j] == '1') { tab[i, j] = 1; }
+                    else { tab[i, j] = 0; }
+                }
+            }
+            GraphMatrix graph = new GraphMatrix(ile, tab);
+            sr.Close();
+            return graph;
         }
         /// <summary>
         /// Zapisuje graf macierzowy do pliku
@@ -25,9 +52,84 @@ namespace Graphs.Actions
         /// <param name="path">sciezka do pliku w ktorym zostanie zapisany z rozszerzeniem *.matrix</param>
         public static void SaveMatrix(GraphMatrix graph, string path)
         {
-            throw new NotImplementedException();
+           // throw new NotImplementedException();
+            StreamWriter sw = new StreamWriter(path);
+            int lim=graph.NodesNr;
+            for (int i=0;i<lim;i++){
+                for (int j = 0; j < lim; j++){
+                    if (graph.GetConnection(i, j)) { sw.Write('1'); }
+                    else sw.Write('0');
+                }
+                sw.Write('\n');
+            }
+            sw.Close();
+            
+
         }
 
         //i 2 inne metody do ladowania 2 innych typow grafow
+
+        public static void SaveMatrixInc(GraphMatrixInc graph, string path)
+        {
+            // throw new NotImplementedException();
+            StreamWriter sw = new StreamWriter(path);
+            int x = graph.NodesNr;
+            int y = graph.ConnectNr; 
+            for (int i = 0; i <x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (graph.GetConnection(i, j)) { sw.Write('1'); }
+                    else sw.Write('0');
+                }
+                sw.Write('\n');
+            }
+            sw.Close();
+
+
+        }
+
+        /// <summary>
+        /// Laduje graf w postaci macierzy węzłów i połączęń z podanego pliku
+        /// </summary>
+        /// <param name="path">Sciezka do pliku z roszerzeniem *.matrixinc</param>
+        /// <returns>Graf macierzowy(węzły,połączenia)</returns>
+
+        public static GraphMatrixInc LoadMatrixInc(string path)
+        {
+            if (!File.Exists(path))
+            {
+
+                return new GraphMatrixInc(0,0);
+            }
+            //throw new NotImplementedException();
+            StreamReader sr = new StreamReader(path);
+            string s = sr.ReadLine();
+            int y = s.Length;
+            int x =0;
+            string[] dane = new string[1000];
+            dane[0] = s;
+            while (dane[x]!= null && (x+1)<1000){
+                dane[x+1]=sr.ReadLine();
+                x++;
+            }
+            int[,] tab=new int[x,y];
+                for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (dane[i][j] == '1') { tab[i, j] = 1; }
+                    else { tab[i, j] = 0; }
+                }
+            }
+            GraphMatrixInc graph = new GraphMatrixInc(x, y, tab);
+            sr.Close();
+            return graph;
+        }
+       
+
+
+
+
     }
 }
