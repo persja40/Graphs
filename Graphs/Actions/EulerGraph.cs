cdfa621170458e6b2a32aa54db74946d2b1172c7
persja40 <persja40@gmail.com>
2016-03-07 16:12:19 +0100

    Euler Graphs

---
+++ Graphs/Actions/EulerGraph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphs.Data;
+using Graphs.Actions;
+
+namespace Graphs.Actions
+{
+    public class EulerGraph
+    {
+        public static GraphMatrix RandEulerGraph(int nodes = 7)
+        {
+            Random r = new Random();
+            List<int> x = new List<int>();
+            while (!Misc.Exists(x))
+            {
+                x.Clear();
+                for (int i = 0; i < nodes; i++)
+                    while (true)
+                    {
+                        int temp = r.Next(nodes - 1);
+                        if (temp % 2 == 0) {
+                            x.Add(temp);
+                            break;
+                        }
+                    }                
+            }
+            return Misc.Construct(x);
+        }
+        public static List<int> EulerianPath() {
+            throw new NotImplementedException();
+        }
+    }
+}
