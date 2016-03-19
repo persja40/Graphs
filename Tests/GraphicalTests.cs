fb390cc8f91926866bb01cab2d5e8bf93c543fc6
Damian <lubiebardzozelki@gmail.com>
2016-03-19 02:27:15 +0100

    Added new test. Zestaw 2.1

---
+++ Tests/GraphicalTests.cs
@@ -0,0 +1,34 @@
+﻿using Graphs.Actions;
+using Graphs.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class GraphicalTests
+    {
+        [TestMethod]
+        public void SimpleGraphicalTest()
+        {
+            Random rand = new Random();
+            for(int i = 0; i < 1000;++i)
+            {
+                GraphMatrix graph = GraphGenerator.generatorGnp(rand.Next(10, 100), 0.5);
+                List<int> sequence = new List<int>();
+
+                for(int node = 0;node < graph.NodesNr;++node)
+                {
+                    sequence.Add(graph.GetNeighbours(node).Count);
+                }
+
+                Assert.IsTrue(Misc.Exists(sequence));
+            }
+        }
+
+    }
+}
