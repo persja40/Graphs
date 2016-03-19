b552e36ed80dd46933f75ffddf2f155193e31aac
Damian <lubiebardzozelki@gmail.com>
2016-03-19 01:42:38 +0100

    Added IndexOfMin method

---
+++ Graphs/Misc/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Misc
+{
+    public static class Utils
+    {
+        public static int IndexOfMin(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentException("Array is null");
+            if (array.Count() == 0)
+                throw new ArgumentException("Array is empty");
+
+            int min = array[0];
+            int minIndex = 0;
+
+            for(int i = 1; i < array.Count(); ++i)
+            {
+                if(array[i] < min)
+                {
+                    min = array[i];
+                    minIndex = i;
+                }
+            }
+
+            return min;
+        }
+    }
+}
