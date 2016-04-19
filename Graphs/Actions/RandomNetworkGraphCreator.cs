2a85ffd76a61327543b9862c06682aad15882ba2
Damian <lubiebardzozelki@gmail.com>
2016-04-19 15:26:39 +0200

    changed maxWeight of nodes to something little smaller.

--- Graphs/Actions/RandomNetworkGraphCreator.cs
+++ Graphs/Actions/RandomNetworkGraphCreator.cs
@@ -98,14 +98,14 @@ namespace Graphs.Actions
             var startNode = rows[0][0];
             foreach(var node in rows[1])
             {
-                connectNodes(startNode, node, int.MaxValue);
+                connectNodes(startNode, node, int.MaxValue - 10000);
             }
 
             var endNode = rows[rows.Count - 1][0];
 
             foreach (var node in rows[rows.Count - 2])
             {
-                connectNodes(node, endNode, int.MaxValue);
+                connectNodes(node, endNode, int.MaxValue - 10000);
             }
         }
 
